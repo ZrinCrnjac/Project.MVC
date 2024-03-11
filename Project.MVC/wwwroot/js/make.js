89b3a968b9779ab678c89dd13ac9d8fe3dca6d64
ZrinCrnjac <zrin.crnjac@gmail.com>
2024-03-11 20:30:38 +0100

    Fix make and model js

---
+++ Project.MVC/wwwroot/js/make.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $('#submitButton').prop('disabled', true);
+
+    $('input[type="text"]').on('keyup', function () {
+        var isValid = true;
+        $('input[type="text"]').each(function () {
+            if ($.trim($(this).val()) === '') {
+                isValid = false;
+                return false;
+            }
+        });
+
+        $('#submitButton').prop('disabled', !isValid);
+    });
+});
--- Project.MVC/wwwroot/js/site.js
+++ Project.MVC/wwwroot/js/site.js
@@ -2,36 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-$(document).ready(function () {
-    $('#submitButton').prop('disabled', true);;
-    var isValidText = false;
-    var isValidSelect = false;
-    $('input[type="text"]').on('keyup', function () {
-        isValidText = true;
-        $('input[type="text"]').each(function () {
-            if ($.trim($(this).val()) === '') {
-                isValidText = false;
-                return false;
-            }
-        });
-
-        $('#submitButton').prop('disabled', !isValidText || !isValidSelect);
-    });
-    $('#MakeId, #MakeId').on('change keyup', function () {
-        isValidSelect = true;
-
-        // Check if the select menu has a selected option
-        if ($('#MakeId').val() === '') {
-            isValidSelect = false;
-        }
-
-        // Check if the input field is empty
-        if ($('#MakeId').val().trim() === '') {
-            isValidSelect = false;
-        }
-
-        // Enable or disable the button based on validation result
-        $('#submitButton').prop('disabled', !isValidText || !isValidSelect);;
-    });
-});
